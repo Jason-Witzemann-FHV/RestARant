71e88f414e2b7ba678aa0dbf5364c321cfc54d3e
Benjamin Mayer <lolgame99@gmail.com>
2022-12-06 08:29:24 +0100

    menu appears when tracker found

--- Assets/PlaceObject.cs
+++ Assets/PlaceObject.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class PlaceObject : MonoBehaviour
 {
     [SerializeField] private Transform _tracker;
+    [SerializeField] private GameObject _menuButtons;
+    [SerializeField] private GameObject _trackerStartingGuide;
 
     // Update is called once per frame
     void Update()
     {
-        if (_tracker != null)
+        if (IsTrackerVisible())
         {
             transform.position = _tracker.position;
         }
     }
 
-    public bool IsTrackervisible()
+    public bool IsTrackerVisible()
     {
         if (_tracker == null)
             return false;
         else
             return true;
+            
+    }
+
+    public void InitialTrackerFound()
+    {
+        _menuButtons.SetActive(true);
+        _trackerStartingGuide.SetActive(false);
     }
 }
